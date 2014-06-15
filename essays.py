fb2200e835ff6daa2b625b0cdef89f8680f4f080
unknown <rlan@L-SHD-00437447.corp.ebay.com>
2014-06-15 22:55:42 +0800

    add LDA model

--- essays.py
+++ essays.py
@@ -71,15 +71,15 @@ if __name__ == '__main__':
     # transform string based documents into tokenID based vectors
     print 'Read Data'
     reader = csv.reader(open("../../dataset/small_samples/project_integrated_data.csv", 'rU'))
+    # reader = csv.reader(open("../../dataset/small_samples/test_data.csv", 'rU'))
     pg2 = project_generator2(reader)
 
     print 'Divide data by resource_type'
 
-    t = 0
-    for projectid, resource_type, doc, train_test, is_exciting in pg2:
-        print 'processing project', t
-        t = t + 1
+    corpus = defaultdict(list)
+    labels = defaultdict(list)
 
+    for projectid, resource_type, doc, train_test, is_exciting in pg2:
         d = dictionary.doc2bow(doc)
         l = 1 if is_exciting == '1' else 0
         
@@ -94,8 +94,9 @@ if __name__ == '__main__':
         corpus[k][:], labels[k][:] = zip(*combined)
 
     # serialize data
-    pickle.dump(corpus, open('corpus.dat', 'wb'))
-    pickle.dump(labels, open('labels.dat', 'wb'))
+    # pickle.dump(corpus, open('corpus.dat', 'wb'))
+    # pickle.dump(labels, open('labels.dat', 'wb'))
+
     # corpus = pickle.load(open('corpus.dat', 'r'))
     # labels = pickle.load(open('labels.dat', 'r'))
 
@@ -103,20 +104,49 @@ if __name__ == '__main__':
     lda_models = {}
     for k in corpus.keys():
         lda_models[k] = models.ldamodel.LdaModel(corpus[k], id2word=dictionary, num_topics=100)
+        lda_models[k].save('lda_models/' + k + '.model')
+
+    # load LDA models
+    for i in os.walk('lda_models'):
+        print i[1] 
 
     print 'Split Train / Test Data'
+    train_corpus = defaultdict(list)
+    test_corpus = defaultdict(list)
+    train_label = defaultdict(list)
+    test_label = defaultdict(list)
+
     train_dat = defaultdict(list)
     test_dat = defaultdict(list)
 
-    random.seed(123456)
-    for k in dataset.keys():
-        random.shuffle(dataset[k])
-        tmp_train = dataset[k][:int(len(dataset)*0.8)]
-        tmp_test = dataset[k][int(len(dataset)*0.8):]
-        train_dat[k] = lda[tmp_train]
-        test_dat[k] = lda[tmp_test]
+    for k in corpus.keys():
+        print k
+        tmp_train = corpus[k][:int(len(corpus[k])*0.8)]
+        tmp_test = corpus[k][int(len(corpus[k])*0.8):]
 
-    print train_dat['Trips']
+        train_corpus[k] = lda_models[k][tmp_train]
+        test_corpus[k] = lda_models[k][tmp_test]
+        train_label[k] = labels[k][:int(len(labels[k])*0.8)]
+        test_label[k] = labels[k][int(len(labels[k])*0.8):]
+        
+        train_dat[k] = [(format_vector_as_dict(d), l) for (d, l) in zip(train_corpus[k].corpus, train_label[k])]
+        test_dat[k] = [(format_vector_as_dict(d), l) for (d, l) in zip(test_corpus[k].corpus, test_label[k])]
+
+    # training phase
+    print 'Start training Naive Bayes Classifier'
+    for k in train_dat.keys():
+        classifier = NaiveBayesClassifier.train(train_dat[k])
+
+        # test the accuracy
+        print 'Testing'
+        results = classifier.batch_classify([fs for (fs, l) in test_dat[k]])
+        correct = [l==r for ((fs,l), r) in zip(test_dat[k], results)]
+        if correct:
+            acc = float(sum(correct))/len(correct)
+        else:
+            acc = 0
+        
+        print k, acc
 
     # translate feature
     # dataset = [(format_vector_as_dict(d), l) for (d, l) in zip(corpus, labels)]
@@ -136,17 +166,4 @@ if __name__ == '__main__':
 
     # split original training set into train / test set.
 
-    # training phase
-    # print 'Start training Naive Bayes Classifier'
-    # classifier = NaiveBayesClassifier.train(train_dat)
-
-    # # test the accuracy
-    # print 'Testing'
-    # results = classifier.batch_classify([fs for (fs, l) in test_dat])
-    # correct = [l==r for ((fs,l), r) in zip(test_dat, results)]
-    # if correct:
-    #     acc = float(sum(correct))/len(correct)
-    # else:
-    #     acc = 0
-
-    # print acc
+    
